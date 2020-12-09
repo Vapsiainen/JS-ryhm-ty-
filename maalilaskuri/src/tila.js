ca6a5a889b14102815f2b635372f97e2910009c9
koodarijesse <jesse.rautakoura@edu.turkuamk.fi>
2020-12-09 11:21:51 +0200

    Lisäsin kommentteja

--- maalilaskuri/src/tila.js
+++ maalilaskuri/src/tila.js
@@ -1,5 +1,7 @@
+// Näitä ei syötetä www-sivuille
+
 const tila = {
     Tilan_nimi: "keittiö",
-    pina_ala: 15,
-    tyyppikerroin: 1
-}
+    pina_ala: 15,               
+    tyyppikerroin: 1       //oletusarvo on 1
+};            
