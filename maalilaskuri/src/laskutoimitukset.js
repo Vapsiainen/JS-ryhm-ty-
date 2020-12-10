2ee62c09124f59291e6579966b9e89e35c935825
Vapsiainen <59833410+Vapsiainen@users.noreply.github.com>
2020-12-10 09:51:10 +0200

    Kommentteja lisätty selventämään

--- maalilaskuri/src/laskutoimitukset.js
+++ maalilaskuri/src/laskutoimitukset.js
@@ -1,16 +1,3 @@
+//Tänne tulee maalin määrän ja hinnan laskutoimitukset.
+
 import React from "react";
-
-function Lähetä() {
-    const sayHello = () => {
-        alert("Hello");
-    }
-    
-    return (
-        <div>
-            <h3>Tämä on komponentti</h3>
-            <button onClick={sayHello}>Click me</button>
-        </div>
-    )
-}
-
-export default Lähetä;
